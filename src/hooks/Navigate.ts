9b9f2f45ba22e91bdd5481979019c64dfd4dc77b
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-17 22:58:09 -0600

    feat: agregar gestión de ubicación en el hook useNavegaciones

--- src/hooks/Navigate.ts
+++ src/hooks/Navigate.ts
@@ -1,14 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const useNavegaciones = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
 
     const goHome = () => {
         navigate("/");
     }
 
     return {
-        goHome
+        goHome,
+        location
+
     }
 }
