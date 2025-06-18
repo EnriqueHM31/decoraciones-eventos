4010f339768798c8fae121ed91155e8cbbb12cdc
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-18 16:54:05 -0600

    fix: eliminar clase de fondo en el componente Altares

--- src/pages/Altares.tsx
+++ src/pages/Altares.tsx
@@ -7,7 +7,7 @@ export default function Altares() {
             <Navegacion />
 
             <section className="flex flex-col items-center justify-center gap-8 py-8">
-                <div className="flex flex-col items-center justify-center gap-6 bg-amber-200">
+                <div className="flex flex-col items-center justify-center gap-6 ">
                     <h1 className="text-4xl font-bold">Altares</h1>
                     <GaleriaAltares />
                 </div>
