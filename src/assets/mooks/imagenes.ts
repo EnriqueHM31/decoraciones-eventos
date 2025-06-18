8ea730679a40ad182bc3aad999ee648ad880cbcb
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-18 16:49:49 -0600

    feat: agregar archivo de imágenes de altares y exportar constantes de imágenes

---
+++ src/assets/mooks/imagenes.ts
@@ -0,0 +1,20 @@
+import IMAGENALTAR1_1 from "@/assets/img/altares/altar1_1.jpg";
+import IMAGENALTAR1_2 from "@/assets/img/altares/altar1_2.jpg";
+import IMAGENALTAR2_1 from "@/assets/img/altares/altar2_1.jpg";
+import IMAGENALTAR2_2 from "@/assets/img/altares/altar2_2.jpg";
+import IMAGENALTAR2_3 from "@/assets/img/altares/altar2_3.jpg";
+import IMAGENALTAR2_4 from "@/assets/img/altares/altar2_4.jpg";
+
+export const IMAGENESALTARES1 = [
+    IMAGENALTAR1_1,
+    IMAGENALTAR1_2,
+];
+
+export const IMAGENESALTARES2 = [
+    IMAGENALTAR2_1,
+    IMAGENALTAR2_2,
+    IMAGENALTAR2_3,
+    IMAGENALTAR2_4,
+    IMAGENALTAR1_1,
+    IMAGENALTAR1_2,
+];
