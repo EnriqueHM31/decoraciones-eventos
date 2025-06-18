6d627963bb06aed90234f845de90b18c37c78f2e
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-17 21:42:32 -0600

    agregar archivo constantes.ts con enlaces y secciones de eventos

---
+++ src/assets/ts/constantes.ts
@@ -0,0 +1,14 @@
+import IMAGENEVENTO from "@/assets/img/Hero.webp";
+
+export const LINKS = [
+    { label: 'Inicio', to: '/' },
+    { label: 'Altares', to: '/altares' },
+    { label: 'Areas Principales', to: '/areas-principales' },
+    { label: 'Eventos', to: '/eventos' },
+]
+
+export const SECCIONES_EVENTOS = [
+    { label: "Altares", img: IMAGENEVENTO, to: "/altares" },
+    { label: "Áreas principales", img: IMAGENEVENTO, to: "/areas" },
+    { label: "Eventos", img: IMAGENEVENTO, to: "/eventos" },
+];
