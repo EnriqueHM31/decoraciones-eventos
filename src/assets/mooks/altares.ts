acf062c88206f3e89047419a76a425ed29b31cf3
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-18 17:40:57 -0600

    feat: actualizar datos de altares con nuevas imágenes y eliminar referencias antiguas

--- src/assets/mooks/altares.ts
+++ src/assets/mooks/altares.ts
@@ -2,58 +2,179 @@ export interface Altar {
     id: number;
     imagenes: string[];
 }
-import { IMAGENESALTARES1, IMAGENESALTARES2 } from "@/assets/mooks/imagenes";
+
 
 export const ALTARESJSON: Altar[] = [
     {
         id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288623/altar1_1_km62ha.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288624/altar1_2_tecxwm.jpg",
+        ]
     },
     {
         id: 2,
         imagenes: [
-            ...IMAGENESALTARES2,
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288624/altar2_1_cbmaxu.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288624/altar2_2_l7pqfn.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288624/altar2_3_uncw64.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288624/altar2_4_f2npin.jpg"
         ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
-    }, {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
-    }, {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
-    }, {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
-    }, {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 3,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288625/altar3_1_g0gm6a.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar3_2_hnp5gk.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar3_3_niejbe.jpg"
+        ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 4,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar4_1_lzwuao.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar4_2_meyad9.jpg",
+        ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
-    }, {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 5,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288575/altar5_1_eqghal.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288575/altar5_2_ti8fsg.jpg"
+        ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 6,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288576/altar6_1_ivkuq5.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar6_2_uj8rl9.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288574/altar6_3_zqqpft.jpg"
+        ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 7,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288576/altar7_1_p2ivsc.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288576/altar7_2_s73oyp.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288577/altar7_3_s8mwk5.jpg"
+        ]
     },
     {
-        id: 1,
-        imagenes: [...IMAGENESALTARES1] // o simplemente: imagenes: IMAGENESALTARES1
+        id: 8,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288576/altar8_1_xgmklb.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288576/altar8_2_xs8tz4.jpg",
+        ]
     },
+    {
+        id: 9,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288577/altar9__licktk.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288577/altar9_2_svvulh.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288577/altar9_3_ybltbd.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288578/altar9_4_tzbpdc.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288578/altar9_5_loeyhm.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288579/altar9_6_kdpyxj.jpg"
+
+        ]
+    },
+    {
+        id: 10,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288579/altar10__kpgacw.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288578/altar10_2_rsgisi.jpg",
+        ]
+    },
+    {
+        id: 11,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288579/altar11_1_zrtwfw.jpg",
+        ]
+    },
+    {
+        id: 12,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288579/altar12_1_hsbzgn.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288579/altar12_2_wktx4g.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288580/altar12_3_wd0l3p.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288580/altar12_4_amxhoz.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288580/altar12_5_f04tiu.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288581/altar12_6_nbfzr4.jpg"
+        ]
+    },
+    {
+        id: 13,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288581/altar13_1_xanlvi.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288581/altar13_2_zjcvyn.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288582/altar13_3_ino0sm.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288582/altar13_4_gejc8v.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288582/altar13_5_rcko2h.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288583/altar13_6_ivecz5.jpg"
+        ]
+    },
+    {
+        id: 14,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288584/altar14_1_mrt7lh.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288582/altar14_2_cxc9rd.jpg"
+        ]
+
+    },
+    {
+        id: 15,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288583/altar15_1_gypwgr.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288583/altar15_2_giubdl.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288583/altar15_3_rv3jjc.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288583/altar15_4_nipzzy.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288584/altar15_6_nzitiw.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288585/altar15_7_emqb2a.jpg"
+        ]
+    },
+    {
+        id: 16,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288585/altar16_1_njgrx2.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288585/altar16_2_rqpdan.jpg"
+        ]
+    },
+    {
+        id: 17,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288585/altar17_1_koow5j.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288585/altar17_2_nkvbeb.jpg"
+        ]
+    },
+    {
+        id: 18,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288586/altar18_1_mvl8bg.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288586/altar18_2_hfjpqr.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288586/altar18_3_pruzen.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar18_4_jsvfem.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar18_5_r13fkr.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar18_6_pynwxu.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar18_7_nwc4w1.jpg"
+        ]
+    },
+    {
+        id: 19,
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar19_1_igfoui.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288587/altar19_2_qbdvej.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288588/altar19_3_oylo0i.jpg",
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750288588/altar19_4_zpxtcu.jpg"
+        ]
+    },
+    {
+        id: 20,
+        imagenes: [
+            ""
+        ]
+    },
+
+
 
 
 
