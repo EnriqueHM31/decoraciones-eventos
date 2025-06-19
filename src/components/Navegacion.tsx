57a7456d9386e761569fa6c682dd630a45481f64
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-18 21:40:15 -0600

    fix: cambiar enlaces de <a> a <li> en Navegacion para mejorar la semántica y accesibilidad

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -34,22 +34,28 @@ export default function Navegacion() {
                                     xl:flex xl:w-auto xl:order-1 xl:static xl:translate-x-0 xl:opacity-100 xl:pointer-events-auto`}
                 >
                     {LINKS.map((link, index) => (
-                        <a
-                            href={link.to}
-                            key={index}
-                            className={`group relative xl:text-xl px-3 xl:py-3 py-2 hover:-translate-y-2.5 duration-500 transition-all  w-full text-center xl:w-fit border-b-4 border-transparent
+                        <li key={index} className={`group relative xl:text-xl px-3 xl:py-3 py-2 hover:-translate-y-2.5 duration-500 transition-all  w-full text-center xl:w-fit border-b-4 border-transparent
                             ${location.pathname === link.to
-                                    ? "bg-white text-primary font-bold  xl:bg-transparent xl:text-white xl:border-white"
-                                    : "text-white"
-                                }`}
-                        >
-                            {link.label}
-                            <span className={`absolute bottom-0 left-0 h-[2px] w-full bg-white scale-x-0 origin-right transition-transform duration-500 ${location.pathname !== link.to ? "group-hover:scale-x-100 group-hover:origin-left" : ""}`}></span>
-                        </a>
+                                ? "bg-white text-primary font-bold  xl:bg-transparent xl:text-white xl:border-white"
+                                : "text-white"
+                            }`}>
+                            <a
+                                href={link.to}
+
+                            >
+                                {link.label}
+                                <span className={`absolute bottom-0 left-0 h-[2px] w-full bg-white scale-x-0 origin-right transition-transform duration-500 ${location.pathname !== link.to ? "group-hover:scale-x-100 group-hover:origin-left" : ""}`}></span>
+                            </a>
+                        </li>
                     ))}
                 </ul>
 
-                <button className="xl:hidden flex" onClick={handleClickOpenMenuBurguer}>
+                <button className="xl:hidden flex" onClick={handleClickOpenMenuBurguer}
+                    title="Abrir menu"
+                    aria-label="Abrir menu"
+                    aria-labelledby="menu-title"
+                    type="button"
+                >
                     {
                         isOpenMenuBurguer ?
                             <IoClose color="white" size={30} />
