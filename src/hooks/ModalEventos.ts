155af157ebb9ba1f712edf5ad65309bbe926b58b
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 16:12:06 -0600

    feat: agregar lógica para manejar el formulario de comentarios en el modal de eventos

--- src/hooks/ModalEventos.ts
+++ src/hooks/ModalEventos.ts
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import { toast } from "sonner";
 
 export const useModalEventos = () => {
     const [visible, setVisible] = useState(false);
     const [isOpenMenuBurguer, setIsOpenMenuBurguer] = useState(false);
+    const [isOpenFormulario, setIsOpenFormulario] = useState(false);
+
+    const handleClickFormulario = () => {
+        setIsOpenFormulario(!isOpenFormulario);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        if (!formData.get("nombre_usuario") || !formData.get("mensaje_usuario")) {
+            toast.error("Por favor, rellena todos los campos");
+            return;
+        }
+
+        toast.success("Gracias por tu comentario");
+        setIsOpenFormulario(false);
+    };
 
 
     const handleViewEventos = () => {
@@ -24,6 +42,9 @@ export const useModalEventos = () => {
         handleNotViewEventos,
         isOpenMenuBurguer,
         handleClickOpenMenuBurguer,
+        isOpenFormulario,
+        handleClickFormulario,
+        handleSubmit,
 
     }
 }
