4df5956d99c9a0fd86bf329864c604d24811993f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 23:01:18 -0600

    feat: agregar hook useFiltros para gestionar la visibilidad del panel lateral

---
+++ src/hooks/Filtros.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function useFiltros() {
+    const [asideVisible, setAsideVisible] = useState(false);
+
+    const toggleAside = () => setAsideVisible((v) => !v);
+    const cerrarAside = () => setAsideVisible(false);
+
+    return {
+        asideVisible,
+        toggleAside,
+        cerrarAside
+    }
+}
