3ac50ea614b3a92d7ef2b27edea632db5f843278
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 23:00:38 -0600

    feat: agregar componente BotonAbrirFiltros para mejorar la funcionalidad de filtros

---
+++ src/components/Filtros/BotonAbrirFiltros.tsx
@@ -0,0 +1,16 @@
+import { FaFilter } from "react-icons/fa";
+
+export default function BotonAbrirFiltros({ toggleAside }: { toggleAside: () => void }) {
+    return (
+        <div className=" justify-end mb-2 w-full max-w-11/12 cursor-pointer">
+            <button
+                onClick={toggleAside}
+                className="xl:p-6 p-5 size-14 xl:size-18 flex items-center justify-center rounded-full bg-primary text-white fixed bottom-4 right-4 xl:bottom-1/12 xl:right-10 z-50"
+            >
+
+
+                <FaFilter className="size-14 text-white" />
+            </button>
+        </div>
+    )
+}
