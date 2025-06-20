8406ee5c73906bd2a10e8bf4e4e88248d8e6684d
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 23:00:05 -0600

    feat: agregar tipos y interfaces para la galería y filtros de género

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,6 @@
+
+type FiltroGenero = "Hombre" | "Mujer" | "Santos" | null;
+
 export interface ImageOverlayGalleryProps {
     images: string[];
     styles?: string;
@@ -14,4 +17,72 @@ export interface AltaresJsonProps {
 interface FormularioEmailProps {
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
     handleClickFormulario: () => void;
+}
+
+export interface Altar {
+    id: number;
+    imagenes: string[];
+    genero: string;
+    colores: string[];
+}
+
+export interface GrupoAltaresProps {
+    altar: Altar;
+    img: string;
+}
+
+export interface EstructuraImagenProps {
+    groupIndex: number;
+    index: number;
+    countPares: number;
+    countImpares: number;
+}
+
+export interface PlaceholdersGridProps {
+    groupIndex: number;
+    grupo: {
+        altar: Altar;
+        img: string;
+    }[]
+}
+
+export interface ImagenGaleriaProps {
+    altar: {
+        altar: Altar;
+        img: string;
+    }
+    index: number;
+    isLazy: boolean;
+    heightClass: string;
+}
+
+interface ImagenesCarruselProps {
+    images: string[];
+}
+
+type IconoGeneroKey = "IoMdMan" | "IoMdWoman" | "GiChurch";
+
+
+export interface GeneroProps {
+    color: string;
+    genero: string;
+    icono: IconoGeneroKey;
+}
+
+interface ColorProps {
+    nombre: string;
+    clase: string;
+    texto: string;
+    bordeCirculo: string;
+}
+
+
+export interface GaleriaStore {
+    filtroGenero: FiltroGenero;
+    filtroColores: string[];
+    setFiltroGenero: (genero: FiltroGenero) => void;
+    toggleColor: (color: string) => void;
+    limpiarFiltros: () => void;
+    removeGenero: () => void;
+    removeColor: (color: string) => void;
 }
