a2f0e14504dd05677c6ffdf33e60c051af327fee
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 23:01:24 -0600

    feat: agregar hook useZonaFiltros para gestionar la visibilidad de filtros de género y colores

---
+++ src/hooks/ZonaFiltros.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import type { ColorProps, GeneroProps } from "@/types";
+
+export function useZonaFiltros() {
+    const [abierto, setAbierto] = useState(false);
+
+    const handleTamaño = () => {
+        if (window.innerWidth < 768) setAbierto(false);
+    }
+
+    const handleAbrirVistaFiltros = () => {
+        setAbierto(true);
+    }
+
+    function handleColorActivoArray(
+        lista: ColorProps[],
+        nombre: string
+    ): { colorActivo?: ColorProps } {
+        const colorActivo = lista.find(
+            (c) => c.nombre.toLowerCase() === nombre.toLowerCase()
+        );
+        return { colorActivo };
+    }
+
+    const handleGeneroLowerCase = (generosDisponibles: GeneroProps[], filtroGenero: string | null) => {
+        return generosDisponibles.find(
+            (g) => g.genero.toLowerCase() === filtroGenero?.toLowerCase()
+        );
+    }
+
+    return {
+        abierto,
+        setAbierto,
+        handleTamaño,
+        handleAbrirVistaFiltros,
+        handleColorActivoArray,
+        handleGeneroLowerCase
+    }
+
+}
