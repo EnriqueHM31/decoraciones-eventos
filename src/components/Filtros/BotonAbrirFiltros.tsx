8f5b6dd93a4e323fb30b2f82ba54c39f77f6942e
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-20 13:26:16 -0600

    feat: ajustar clases de estilo en BotonAbrirFiltros para mejorar la responsividad

--- src/components/Filtros/BotonAbrirFiltros.tsx
+++ src/components/Filtros/BotonAbrirFiltros.tsx
@@ -5,10 +5,8 @@ export default function BotonAbrirFiltros({ toggleAside }: { toggleAside: () => 
         <div className=" justify-end mb-2 w-full max-w-11/12 cursor-pointer">
             <button
                 onClick={toggleAside}
-                className="xl:p-6 p-5 size-14 xl:size-18 flex items-center justify-center rounded-full bg-primary text-white fixed bottom-4 right-4 xl:bottom-1/12 xl:right-10 z-50"
+                className="xl:p-6 p-5 size-14 md:size-18 flex items-center justify-center rounded-full bg-primary text-white fixed bottom-4 right-4 md:bottom-1/12 md:right-10 z-50"
             >
-
-
                 <FaFilter className="size-14 text-white" />
             </button>
         </div>
