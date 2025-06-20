c3441afa7b15ab94adb3f7eca52b29a6ff76aae5
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-19 18:22:21 -0600

    feat: agregar componente Filtros en la página Altares

--- src/pages/Altares.tsx
+++ src/pages/Altares.tsx
@@ -1,5 +1,6 @@
 import Navegacion from "@/components/Navegacion";
 import GaleriaAltares from "@/components/GaleriaImagenes";
+import Filtros from "@/components/Filtros";
 
 export default function Altares() {
     return (
@@ -7,6 +8,7 @@ export default function Altares() {
             <Navegacion />
 
             <section className="flex flex-col items-center justify-center gap-8 py-10 px-2 w-full ">
+                <Filtros />
                 <GaleriaAltares />
             </section>
 
