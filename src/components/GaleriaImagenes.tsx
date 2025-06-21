b4fb8ff3176c896c5ffef881ef32d1f6f8a029a2
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 14:50:21 -0600

    feat: eliminar consola de depuración en GaleriaAltares

--- src/components/GaleriaImagenes.tsx
+++ src/components/GaleriaImagenes.tsx
@@ -6,12 +6,9 @@ import ImagenGaleria from "./Galeria/ImagenGaleria";
 
 export default function GaleriaAltares() {
     const { columnas, EstructuraImagen } = useGaleria();
-    console.log(columnas);
     const totalImagenes = columnas.reduce((total, columna) => total + columna.length, 0);
     const GridLaptopMax8 = totalImagenes < 9 && totalImagenes > 0 ? "lg:grid-cols-2" : "lg:grid-cols-4"
     const GridAltoMax4 = totalImagenes < 4 && totalImagenes > 0 ? "min-h-[80dvh]" : ""
-
-
 
     return (
         <>
