77a838dd6f4e3813dea171dae73760f2c16dd728
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-20 21:39:15 -0600

    feat: agregar funcionalidad de WhatsApp y mejorar la estructura del carrusel

--- src/components/Carrusel.tsx
+++ src/components/Carrusel.tsx
@@ -1,23 +1,21 @@
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselPrevious,
-    CarouselNext,
-} from "@/components/ui/carousel";
-
-import { Card, CardContent } from "@/components/ui/card";
-import { useCarrousel } from "@/hooks/Carrusel";
 import type { ImagenesCarruselProps } from "@/types";
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import { FaWhatsapp } from "react-icons/fa6";
+import Previsualizacion from "./WhatSapp/Previsualizacion";
+import { useWhatsapp } from "@/hooks/WhatSapp";
+import { useCarrousel } from "@/hooks/Carrusel";
+
 
 
 export default function SliderImagenes({ images }: ImagenesCarruselProps) {
     const { current, count, setApi } = useCarrousel({ images });
+    const { modalOpen, handleClickWhatsapp, handleMensaje, handleClickCloseWhatsapp, mensaje, imagenSeleccionada } = useWhatsapp({ images, current });
 
     return (
         <>
-            <Carousel setApi={setApi} className="w-full py-0 bg-transparent border-none">
-                <CarouselContent className="py-0 bg-transparent border-none">
+            <Carousel setApi={setApi} className="w-full py-0 bg-transparent border-none ">
+                <CarouselContent className="py-0 bg-transparent border-none ">
                     {images.map((image, index) => (
                         <CarouselItem key={index}>
                             <Card className="w-full h-full py-0 rounded-lg flex items-center justify-center bg-transparent border-none">
@@ -31,6 +29,7 @@ export default function SliderImagenes({ images }: ImagenesCarruselProps) {
                                 </CardContent>
                             </Card>
                         </CarouselItem>
+
                     ))}
                 </CarouselContent>
 
@@ -41,6 +40,17 @@ export default function SliderImagenes({ images }: ImagenesCarruselProps) {
             <div className=" absolute bottom-6 left-1/2 flex -translate-x-1/2 text-white md:text-2xl bg-primary  font-bold px-5 py-2 rounded-full">
                 Slide {current} de {count}
             </div>
+
+            <button
+                onClick={handleClickWhatsapp}
+                className="btn bg-green-600 rounded-full text-white p-4 fixed bottom-1/12 right-7 md:right-12 z-30 cursor-pointer"
+            >
+                <FaWhatsapp className="md:size-12 size-7 text-white" />
+            </button>
+            {/* Modal */}
+            {modalOpen && (
+                <Previsualizacion mensaje={mensaje} imagenSeleccionada={imagenSeleccionada} handleMensaje={handleMensaje} handleClickCloseWhatsapp={handleClickCloseWhatsapp} />
+            )}
         </>
     );
 }
