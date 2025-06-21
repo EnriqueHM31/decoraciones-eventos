4da70f60ccc289e62fdd7ebf8f94968f9f109d34
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 14:50:33 -0600

    feat: agregar página de Eventos con componentes TituloPagina y Navegacion

---
+++ src/pages/Eventos.tsx
@@ -0,0 +1,11 @@
+import Navegacion from "@/components/Navegacion";
+import TituloPagina from "@/components/ui/TituloPagina";
+
+export default function Eventos() {
+    return (
+        <>
+            <TituloPagina titulo="Eventos" />
+            <Navegacion />
+        </>
+    )
+}
