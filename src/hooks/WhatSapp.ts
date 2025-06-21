a28e39351a26bb5f5515ded3c24366694480dc33
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-20 21:39:44 -0600

    feat: agregar hook useWhatsapp para manejar la lógica de WhatsApp

---
+++ src/hooks/WhatSapp.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface HooksWhatsappProps {
+    images: string[];
+    current: number;
+}
+
+export function useWhatsapp({ images, current }: HooksWhatsappProps) {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [ImagenWhat, setImagenWhat] = useState(0);
+    const [mensaje, setMensaje] = useState("¡Hola! Me interesa este arreglo:");
+    const imagenSeleccionada = images[ImagenWhat ?? 0];
+
+    const handleClickWhatsapp = () => {
+        const index = current - 1;
+        setImagenWhat(index);
+        setModalOpen(true);
+    }
+
+    const handleMensaje = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMensaje(e.target.value);
+    };
+
+    const handleClickCloseWhatsapp = () => {
+        setModalOpen(false);
+    }
+    return {
+        modalOpen,
+        handleClickWhatsapp,
+        handleMensaje,
+        handleClickCloseWhatsapp,
+        mensaje,
+        imagenSeleccionada
+    }
+}
