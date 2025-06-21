59c29d54785f452fd0e968a3fae8280cf546dd44
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 14:50:27 -0600

    feat: reorganizar estructura del componente AreasPrincipales para incluir TituloPagina y Navegacion

--- src/pages/AreasPrincipales.tsx
+++ src/pages/AreasPrincipales.tsx
@@ -1,20 +1,31 @@
+import Navegacion from "@/components/Navegacion";
+import TituloPagina from "@/components/ui/TituloPagina";
+
 export default function AreasPrincipales() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold">
-                ¡Hola! 👋
-            </h1>
-            <p className="text-xl">
-                Bienvenido a nuestra aplicación de decoración de eventos.
-            </p>
-            <p className="text-xl">
-                Esta es una aplicación web que te permite encontrar y ver diferentes decoraciones de eventos.
-            </p>
-            <p className="text-xl">
-                ¡Esperamos que disfrutes de nuestra aplicación y que te resulte útil!
-                😊
-            </p>
+        <>
+            <TituloPagina titulo="Areas Principales" />
 
-        </div>
+            <Navegacion />
+
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-3xl font-bold">
+                    ¡Hola! 👋
+                </h1>
+                <p className="text-xl">
+                    Bienvenido a nuestra aplicación de decoración de eventos.
+                </p>
+                <p className="text-xl">
+                    Esta es una aplicación web que te permite encontrar y ver diferentes decoraciones de eventos.
+                </p>
+                <p className="text-xl">
+                    ¡Esperamos que disfrutes de nuestra aplicación y que te resulte útil!
+                    😊
+                </p>
+
+            </div>
+
+
+        </>
     );
 }
