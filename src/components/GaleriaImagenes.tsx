3b80af36a63bfd4f82c3d82d9b837ddb2cb0b474
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 22:21:07 -0600

    feat: actualizar GaleriaImagenes para recibir props y manejar altares y decoraciones

--- src/components/GaleriaImagenes.tsx
+++ src/components/GaleriaImagenes.tsx
@@ -2,10 +2,13 @@ import SinImagenes from "./Galeria/SinImagenes";
 import ZonaFiltros from "./Galeria/ZonaFiltros";
 import { useGaleria } from "@/hooks/Galeria";
 import ImagenGaleria from "./Galeria/ImagenGaleria";
+import type { GaleriaProps } from "@/types";
 
 
-export default function GaleriaAltares() {
-    const { columnas, EstructuraImagen } = useGaleria();
+
+
+export default function GaleriaImagenes({ MOOKS, tipo }: GaleriaProps) {
+    const { columnas, EstructuraImagen, esAltar } = useGaleria({ MOOKS, tipo });
     const totalImagenes = columnas.reduce((total, columna) => total + columna.length, 0);
     const GridLaptopMax8 = totalImagenes < 9 && totalImagenes > 0 ? "lg:grid-cols-2" : "lg:grid-cols-4"
     const GridAltoMax4 = totalImagenes < 4 && totalImagenes > 0 ? "min-h-[80dvh]" : ""
@@ -23,16 +26,23 @@ export default function GaleriaAltares() {
             <div className={`grid grid-cols-1 sm:grid-cols-2 ${GridLaptopMax8}  ${GridAltoMax4} gap-4 md:max-w-11/12 w-full`}>
                 {columnas.map((columna, groupIndex) => (
                     <div key={groupIndex} className="flex flex-col gap-4">
-                        {columna.map((altarData, index) => {
-                            const { heightClass, isLazy } = EstructuraImagen({ groupIndex, index });
+                        {columna.map((item, index) => {
+                            const { heightClass, isLazy } = EstructuraImagen({
+                                groupIndex,
+                                index,
+                                arrayGaleria: columna,
+                            });
+
+                            const galeria = item.galeria; // item debe ser de tipo GaleriaColumnasProps
+
                             return (
                                 <ImagenGaleria
                                     key={index}
-                                    altar={altarData}
+                                    altar={esAltar(galeria) ? galeria : undefined}
+                                    decoracion={!esAltar(galeria) ? galeria : undefined}
                                     index={index}
                                     heightClass={heightClass}
                                     isLazy={isLazy}
-
                                 />
                             );
                         })}
