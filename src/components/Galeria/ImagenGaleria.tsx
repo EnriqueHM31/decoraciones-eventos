f4abac21ae85fe623e71314e88a801bc7b621f7f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 22:21:10 -0600

    feat: agregar soporte para decoraciones en el componente ImagenGaleria

--- src/components/Galeria/ImagenGaleria.tsx
+++ src/components/Galeria/ImagenGaleria.tsx
@@ -3,25 +3,56 @@ import SliderImagenes from "@/components/SliderImagenes";
 import type { ImagenGaleriaProps } from "@/types";
 
 
-export default function ImagenGaleria({ altar, index, isLazy, heightClass }: ImagenGaleriaProps) {
+export default function ImagenGaleria({ altar, decoracion, index, isLazy, heightClass }: ImagenGaleriaProps) {
     return (
-        <motion.div
-            key={index}
-            initial={{ opacity: 0, scale: 0.9, y: 50, transition: { duration: 0.5 } }}
-            whileInView={{ opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } }}
-            transition={{
-                duration: 0.5,
-                ease: "easeOut",
-                delay: index * 0.1,
-            }}
-            viewport={{ once: true, amount: 0.3 }}
-            className={`w-full rounded-lg overflow-hidden ${heightClass}`}
-        >
-            <SliderImagenes
-                images={altar.altar.imagenes}
-                styles="h-full w-full"
-                lazy={isLazy}
-            />
-        </motion.div>
+        <>
+            {
+                altar && (
+
+                    <motion.div
+                        key={index}
+                        initial={{ opacity: 0, scale: 0.9, y: 50, transition: { duration: 0.5 } }}
+                        whileInView={{ opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } }}
+                        transition={{
+                            duration: 0.5,
+                            ease: "easeOut",
+                            delay: index * 0.1,
+                        }}
+                        viewport={{ once: true, amount: 0.3 }}
+                        className={`w-full rounded-lg overflow-hidden ${heightClass}`}
+                    >
+                        <SliderImagenes
+                            images={altar.imagenes}
+                            styles="h-full w-full"
+                            lazy={isLazy}
+                        />
+                    </motion.div>
+                )
+
+            }
+            {
+                decoracion && (
+                    <motion.div
+                        key={index}
+                        initial={{ opacity: 0, scale: 0.9, y: 50, transition: { duration: 0.5 } }}
+                        whileInView={{ opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } }}
+                        transition={{
+                            duration: 0.5,
+                            ease: "easeOut",
+                            delay: index * 0.1,
+                        }}
+                        viewport={{ once: true, amount: 0.3 }}
+                        className={`w-full rounded-lg overflow-hidden ${heightClass}`}
+                    >
+                        <SliderImagenes
+                            images={decoracion.imagenes}
+                            styles="h-full w-full"
+                            lazy={isLazy}
+                        />
+                    </motion.div>
+                )
+            }
+
+        </>
     )
 }
