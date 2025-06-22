d6c033ae4018682610199e5aa5b33912cf7195fb
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 22:20:48 -0600

    fix: corregir la ruta del componente de eventos a decoraciones en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Altares from "./pages/Altares"
 import Footer from "@/components/Footer"
 import { Toaster } from "sonner";
 import AreasPrincipales from "./pages/AreasPrincipales"
-import Eventos from "./pages/Eventos"
+import Eventos from "./pages/Decoraciones"
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/*" element={<Error404 />} />
         <Route path="/altares" element={<Altares />} />
         <Route path="/areas-principales" element={<AreasPrincipales />} />
-        <Route path="/eventos" element={<Eventos />} />
+        <Route path="/decoraciones" element={<Eventos />} />
       </Routes>
 
       <Footer />
