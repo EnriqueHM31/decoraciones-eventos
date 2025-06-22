9aa7712beb6aa7c4040c24010f3c842fe00bdb56
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-22 16:22:28 -0600

    feat: agregar componente BotonMenu en la página de Decoraciones

--- src/pages/Decoraciones.tsx
+++ src/pages/Decoraciones.tsx
@@ -2,6 +2,8 @@ import GaleriaImagenes from "@/components/GaleriaImagenes";
 import Navegacion from "@/components/Navegacion";
 import TituloPagina from "@/components/ui/TituloPagina";
 import { DECORACIONESJSON } from "@/assets/mooks/decoraciones";
+import BotonMenu from "@/components/Filtros/BotonAbrirFiltros";
+
 
 export default function Eventos() {
 
@@ -10,10 +12,13 @@ export default function Eventos() {
         <>
             <TituloPagina titulo="Decoraciones" />
             <Navegacion />
+            <BotonMenu tipo="altares" />
 
             <section className="flex flex-col items-center justify-center gap-1 md:gap-7 py-2 px-2 w-full ">
                 <GaleriaImagenes MOOKS={MOOKS} tipo="decoraciones" />
             </section>
+
+
         </>
     )
 }
