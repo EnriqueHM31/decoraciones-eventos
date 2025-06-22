4acdd999e014538c337e71fbfb530722964ccab6
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-21 22:21:17 -0600

    feat: actualizar el componente Altares para utilizar GaleriaImagenes con datos de altares

--- src/pages/Altares.tsx
+++ src/pages/Altares.tsx
@@ -1,15 +1,17 @@
 import Navegacion from "@/components/Navegacion";
-import GaleriaAltares from "@/components/GaleriaImagenes";
+import GaleriaImagenes from "@/components/GaleriaImagenes";
 import Filtros from "@/components/Filtros";
+import { ALTARESJSON } from "@/assets/mooks/altares";
 
 export default function Altares() {
+    const MOOKS = ALTARESJSON;
     return (
         <>
             <Navegacion />
 
             <section className="flex flex-col items-center justify-center gap-1 md:gap-7 py-2 px-2 w-full ">
                 <Filtros />
-                <GaleriaAltares />
+                <GaleriaImagenes MOOKS={MOOKS} tipo="altares" />
             </section>
 
         </>
