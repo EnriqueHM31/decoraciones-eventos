f03ba60c1ca0972bc16b56815d93f2b18436ad7a
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 16:49:09 -0600

    refactor: eliminar contenido estático y agregar galería de imágenes en AreasPrincipales

--- src/pages/AreasPrincipales.tsx
+++ src/pages/AreasPrincipales.tsx
@@ -1,5 +1,7 @@
+import GaleriaImagenes from "@/components/GaleriaImagenes";
 import Navegacion from "@/components/Navegacion";
 import TituloPagina from "@/components/TituloPagina";
+import { AREASPRINCIPALESJSON } from "@/assets/mooks/Areas-principales";
 
 export default function AreasPrincipales() {
     return (
@@ -8,22 +10,10 @@ export default function AreasPrincipales() {
 
             <Navegacion />
 
-            <div className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-3xl font-bold">
-                    ¡Hola! 👋
-                </h1>
-                <p className="text-xl">
-                    Bienvenido a nuestra aplicación de decoración de eventos.
-                </p>
-                <p className="text-xl">
-                    Esta es una aplicación web que te permite encontrar y ver diferentes decoraciones de eventos.
-                </p>
-                <p className="text-xl">
-                    ¡Esperamos que disfrutes de nuestra aplicación y que te resulte útil!
-                    😊
-                </p>
+            <section className="flex flex-col items-center justify-center gap-1 md:gap-7 py-2 px-2 w-full ">
+                <GaleriaImagenes MOOKS={AREASPRINCIPALESJSON} tipo="areas-principales" />
+            </section>
 
-            </div>
 
 
         </>
