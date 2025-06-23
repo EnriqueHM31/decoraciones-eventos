a49d880dd15594d2f6e2eca508d7b031a70bbd1c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 12:15:12 -0600

    feat: agregar componente ZonaButtonQuitarFiltros para limpiar filtros

---
+++ src/components/Buttons/ZonaButtonQuitarFiltros.tsx
@@ -0,0 +1,22 @@
+import { useZonaFiltros } from "@/hooks/ZonaFiltros";
+import { useGaleriaStore } from "@/store/useGaleriaStore";
+
+export default function ZonaButtonQuitarFiltros() {
+
+    const { limpiarFiltros } = useGaleriaStore();
+    const { handleTamaño } = useZonaFiltros();
+    return (
+        <button
+            onClick={() => {
+                limpiarFiltros();
+                handleTamaño();
+            }}
+            className="flex items-center gap-2 mt-4 md:mt-0 px-4 py-2 rounded-md text-sm border border-black/70 text-white bg-primary hover:bg-gray-500 transition-colors duration-500 ease-in-out"
+            title="Quitar filtros"
+            aria-label="Quitar filtros"
+            type="button"
+        >
+            Quitar filtros
+        </button>
+    )
+}
