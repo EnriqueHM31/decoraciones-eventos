0357d56ac03d774a9c22252e87fa43de2f8091bc
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 16:48:55 -0600

    feat: agregar componente ImagenOptimizada para carga diferida de imágenes

---
+++ src/components/Galeria/ImagenOptimizada.tsx
@@ -0,0 +1,20 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+
+interface ImagenOptimizadaProps {
+    url: string;
+    alt: string;
+    clases: string;
+}
+
+export default function ImagenOptimizada({ url, alt, clases }: ImagenOptimizadaProps) {
+    return (
+        <LazyLoadImage
+            src={url}
+            alt={alt}
+            className={clases}
+            aria-label={alt}
+            title={alt}
+        />
+    );
+}
