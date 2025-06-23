8a105cfef0a6724b1852f6250dba7ce6458141cc
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 16:48:30 -0600

    feat: agregar archivo AreasPrincipales con datos de imágenes

---
+++ src/assets/mooks/Areas-principales.ts
@@ -0,0 +1,207 @@
+import type { AreaPrincipal } from "@/types";
+
+export const AREASPRINCIPALESJSON: AreaPrincipal[] = [
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_21_k4jgxd.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_20_wpgdt1.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712470/areas-principal_1_zz0ncs.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_19_vlpqzo.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_18_px3zly.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712471/areas-principal_4_iah7cc.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_40_jmkvvq.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712470/areas-principal_3_utf4az.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_39_qrm0jp.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_38_dtqkkm.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_37_vmvknn.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_36_yy2zye.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_35_qnthkm.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_34_axfbg1.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712483/areas-principal_33_lawhii.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712482/areas-principal_32_te2wh0.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712482/areas-principal_31_pwrfup.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712482/areas-principal_30_o5tp5q.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712470/areas-principal_2_naf2g5.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712475/areas-principal_29_ut8pjw.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712474/areas-principal_28_vvai2e.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712474/areas-principal_27_znzguy.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712474/areas-principal_26_bk4u7x.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712474/areas-principal_25_avitri.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712473/areas-principal_24_goxo3z.webp"
+        ]
+    },
+
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712474/areas-principal_23_eljsp6.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712473/areas-principal_22_rmounf.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_21_k4jgxd.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_20_wpgdt1.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712470/areas-principal_1_zz0ncs.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_19_vlpqzo.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_18_px3zly.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_17_sritk1.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_16_btb6vg.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_15_ajtxri.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_14_rc2bsa.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712472/areas-principal_13_muyqcw.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712471/areas-principal_12_ehjcea.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712471/areas-principal_11_qlnuwm.webp"
+        ]
+    },
+    {
+        imagenes: [
+            "https://res.cloudinary.com/dovznesem/image/upload/v1750712470/areas-principal_10_ypgdjl.webp"
+        ]
+    }
+
+
+]
