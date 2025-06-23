5eb5488d79648ddb62a83d7b203d24ef72d3dcf9
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 16:49:00 -0600

    fix: reemplazar etiqueta img por componente ImagenOptimizada en Previsualizacion

--- src/components/WhatSapp/Previsualizacion.tsx
+++ src/components/WhatSapp/Previsualizacion.tsx
@@ -1,5 +1,5 @@
-import { IoClose } from "react-icons/io5";
 import type { PrevisualizacionMensajeWhatsappProps } from "@/types";
+import ImagenOptimizada from "../Galeria/ImagenOptimizada";
 
 
 export default function Previsualizacion({ mensaje, imagenSeleccionada, handleMensaje, handleClickCloseWhatsapp }: PrevisualizacionMensajeWhatsappProps) {
@@ -10,25 +10,24 @@ export default function Previsualizacion({ mensaje, imagenSeleccionada, handleMe
     )}`;
 
     return (
-        <div className="fixed h-screen w-screen top-0 left-0  backdrop-blur-xl bg-opacity-70 flex justify-center items-center z-100">
+        <div className="fixed h-screen w-screen top-0 left-0 justify-center backdrop-blur-xl flex items-center z-120 bg-black/70">
 
-            <div className="bg-black md:border md:border-white rounded-lg md:max-w-4xl w-full flex overflow-hidden md:flex-row flex-col justify-center h-full md:max-h-[50dvh]">
+            <div className=" md:border md:border-white rounded-lg md:max-w-4xl w-full flex overflow-hidden md:flex-row flex-col  h-full md:max-h-[50dvh] justify-center overflow-y-auto">
                 {/* Izquierda: Formulario */}
-                <div className="md:w-1/2 p-6 flex flex-col justify-center items-center md:items-start md:justify-start md:order-1 order-2">
+                <div className="md:w-1/2 p-6 flex flex-col justify-center items-center md:items-start md:justify-start md:order-1 order-2 bg-black">
 
                     <h2 className="text-md md:text-xl font-bold mb-4 text-white">Editar mensaje</h2>
                     <textarea
-                        className="text-sm md:text-md border rounded p-2 flex-grow resize-none text-white w-full"
+                        className="text-sm md:text-md border rounded p-2 flex-grow resize-none text-white w-full h-full min-h-34 "
                         value={mensaje}
                         onChange={(e) => handleMensaje(e)}
-                        rows={10}
                         name="mensaje_usuario"
                         id="mensaje_usuario"
                     />
 
-                    <div className="mt-4 flex justify-between w-full items-center order-1 md:order-2">
+                    <div className="mt-4 flex flex-col md:flex-row gap-3 md:gap-0 justify-between w-full items-center order-1 md:order-2">
                         <button
-                            className="md:bg-gray-300 md:px-4 md:py-2 p-1 size-12 md:w-fit fixed md:static md:top-4 md:right-4 flex items-center justify-center md:rounded rounded-full hover:bg-gray-400 cursor-pointer z-60"
+                            className="bg-gray-300 px-4 py-2 w-full md:w-fit  md:static md:top-4 md:right-4 flex items-center justify-center rounded hover:bg-gray-400 order-2 md:order-1 cursor-pointer z-60"
                             onClick={handleClickCloseWhatsapp}
                             name="cancelar"
                             id="cancelar"
@@ -36,14 +35,13 @@ export default function Previsualizacion({ mensaje, imagenSeleccionada, handleMe
                             title="Cancelar modal"
                             type="button"
                         >
-                            <span className="md:flex hidden">Cancelar</span>
-                            <IoClose className="md:hidden text-white w-full size-12" />
+                            <span className="">Cancelar</span>
                         </button>
                         <a
                             href={whatsAppLink}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="bg-green-600 w-full text-center md:w-fit text-white px-4 py-2 rounded hover:bg-green-700"
+                            className="bg-green-600 w-full text-center md:w-fit text-white px-4 py-2 rounded hover:bg-green-700 order-1 md:order-2"
                             onClick={handleClickCloseWhatsapp}
                             id="enviar"
                             aria-label="Enviar modal"
@@ -56,12 +54,8 @@ export default function Previsualizacion({ mensaje, imagenSeleccionada, handleMe
                 </div>
 
                 {/* Derecha: Previsualización imagen */}
-                <div className="md:w-1/2 bg-black backdrop-blur-3xl flex w-full items-center justify-center md:p-6 md:order-2 order-1">
-                    <img
-                        src={imagenSeleccionada}
-                        alt="Previsualización del arreglo"
-                        className="md:max-h-[70vh] max-h-[25dvh] object-contain rounded w-full h-full"
-                    />
+                <div className="md:w-1/2 h-2/6 md:h-full flex w-full items-center justify-center md:p-6 md:order-2 order-1 bg-black ">
+                    <ImagenOptimizada url={imagenSeleccionada} alt="Previsualización del arreglo" clases="md:max-h-[70vh] object-contain rounded w-full h-full" />
                 </div>
             </div>
         </div>
