2b088579e7fffe5e1e7368bf2611bb8e779d43cc
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 11:46:04 -0600

    feat: agregar título a la página Altares

--- src/pages/Altares.tsx
+++ src/pages/Altares.tsx
@@ -2,11 +2,13 @@ import Navegacion from "@/components/Navegacion";
 import GaleriaImagenes from "@/components/GaleriaImagenes";
 import Filtros from "@/components/Filtros";
 import { ALTARESJSON } from "@/assets/mooks/altares";
+import TituloPagina from "@/components/TituloPagina";
 
 export default function Altares() {
     const MOOKS = ALTARESJSON;
     return (
         <>
+            <TituloPagina titulo="Altares" />
             <Navegacion />
 
             <section className="flex flex-col items-center justify-center gap-1 md:gap-7 py-2 px-2 w-full ">
