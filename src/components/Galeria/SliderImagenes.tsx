78162761d6f0fce2b42ecb7eafac9fc02a4387d2
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-23 18:42:23 -0600

    fix: ajustar contenedor y clases de estilo en SliderImagenes y Galeria para mejorar la presentación

--- src/components/Galeria/SliderImagenes.tsx
+++ src/components/Galeria/SliderImagenes.tsx
@@ -18,19 +18,23 @@ export default function SliderImagenes({ lazy, images, styles }: ImageOverlayGal
     return (
         <>
             {/* Imagen en el grid, siempre visible */}
-            <motion.img
-                src={images[0]}
-                alt="Imagen principal de la galería"
-                onClick={() => {
-                    setCurrentIndex(0);
-                    handleImageSelected(images[0], 0);
-                }}
-                loading={lazy ? "lazy" : undefined}
-                crossOrigin="anonymous"
-                className={`w-50dvw h-screen object-cover object-center cursor-pointer ${styles ?? ""}`}
-                whileTap={{ scale: 0.95 }}
-                layoutId={`imagen-${images[0]}`}
-            />
+            <div className="w-full h-full overflow-hidden rounded-lg">
+                <motion.img
+                    src={images[0]}
+                    alt="Imagen principal de la galería"
+                    onClick={() => {
+                        setCurrentIndex(0);
+                        handleImageSelected(images[0], 0);
+                    }}
+                    loading={lazy ? "lazy" : undefined}
+                    crossOrigin="anonymous"
+                    width={400}
+                    height={400}
+                    className={`w-full h-full object-cover object-center cursor-pointer ${styles ?? ""}`}
+                    whileTap={{ scale: 0.95 }}
+                    layoutId={`imagen-${images[0]}`}
+                />
+            </div>
 
 
             {/* Modal animado */}
